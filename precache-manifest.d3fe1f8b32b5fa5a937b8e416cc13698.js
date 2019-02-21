8612c18b65ad23d1a4783f4a9c2835be283ec526
Roberto <robertmsoriano@gmail.com>
2019-02-20 23:49:27 -0500

    Updates

---
+++ precache-manifest.d3fe1f8b32b5fa5a937b8e416cc13698.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd4d977d90abf4081e3b",
+    "url": "https//robertomsoriano.github.io/npi-lookup/static/js/runtime~main.dd4d977d.js"
+  },
+  {
+    "revision": "99a18b25689b70230e33",
+    "url": "https//robertomsoriano.github.io/npi-lookup/static/js/main.99a18b25.chunk.js"
+  },
+  {
+    "revision": "aaa5e7ac662b5ac18b38",
+    "url": "https//robertomsoriano.github.io/npi-lookup/static/js/2.aaa5e7ac.chunk.js"
+  },
+  {
+    "revision": "99a18b25689b70230e33",
+    "url": "https//robertomsoriano.github.io/npi-lookup/static/css/main.10b01859.chunk.css"
+  },
+  {
+    "revision": "ffb83b56f625261d553793053f353319",
+    "url": "https//robertomsoriano.github.io/npi-lookup/index.html"
+  }
+];
